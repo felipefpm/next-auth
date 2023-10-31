1ebe8a0b5b030d1e88fa5a29e081474eaa0cbdec
Felipe Pinto <felipepinto.fpm@gmail.com>
2023-10-31 17:37:26 -0300

    WIP

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../context/AuthContext"
-import { api } from "../services/api"
+import { api } from "../services/apiClient"
+import { withSSRAuth } from "../utils/withSSRAuth"
+import { setupAPIClient } from "../services/api"
 
 export default function Dashboard() {
 
@@ -21,3 +23,12 @@ export default function Dashboard() {
     <h1>DASHBOARD: {user?.email}</h1>
   )
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx)
+  const response = await apiClient.get('/me')
+
+  return {
+    props: {}
+  }
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { FormEvent, useContext, useState } from "react"
 import { AuthContext } from "../context/AuthContext"
+import { GetServerSideProps } from "next"
+import { withSSRGuest } from "../utils/withSSRGuest"
 
 export default function Home() {
   const [email, setEmail] = useState('')
@@ -26,3 +28,9 @@ export default function Home() {
     </form>
   )
 }
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {}
+  }
+})
